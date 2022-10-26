4bd97707aa145176c4ca3a2040a436ccf5416704
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-10-27 00:45:12 +0600

    create course details api

--- index.js
+++ index.js
@@ -22,6 +22,12 @@ app.get('/category/:id', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courses);
 })
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(req);
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
     console.log('tutorial server running on port', port);
